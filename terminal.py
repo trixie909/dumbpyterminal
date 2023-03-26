35f73305a63b47f3b2bf7754a62edea44e80c9e2
trixie909 <99223145+trixie909@users.noreply.github.com>
2023-03-26 09:29:19 -0500

    new
    many bugs squashed + better math

--- terminal.py
+++ terminal.py
@@ -21,8 +21,11 @@ while True:
             print("you can't write to a file after write is broken/if you never created one silly")
             break
     elif userinput == "breakwrite":
-        f.close()
-        print("file closed, write operations no longer usable")
+        try:    
+            f.close()
+            print("file closed, write operations no longer usable")
+        except:
+            print("you can't breakwrite when you never created a file")
     elif userinput == "exit":
         break
     elif userinput == "whichos":
@@ -34,6 +37,7 @@ while True:
             filenametoread = input("which file to read? (include the extension): ")
             file1 = open(filenametoread, "r")
             print(file1.read())
+            file1.close()
         except:
             print("cannot read file that does not exist/write not broken")
     elif userinput == "deletefile":
@@ -45,55 +49,55 @@ while True:
             print("file does not exist/deletion error")
     elif userinput == "addition":
         try:
-            add1 = int(input("enter first number: "))
-            add2 = int(input("enter second number: "))
+            add1 = float(input("enter first number: "))
+            add2 = float(input("enter second number: "))
             sum = add1 + add2
             print(sum)
         except:
             print("enter a NUMBER")
     elif userinput == "subtraction":
         try:
-            sub1 = int(input("enter first number: "))
-            sub2 = int(input("enter second number: "))
+            sub1 = float(input("enter first number: "))
+            sub2 = float(input("enter second number: "))
             diff = sub1-sub2
             print(diff)
         except:
             print("enter a number")
     elif userinput == "multiply":
         try:
-            mult1 = int(input("enter first number: "))
-            mult2 = int(input("enter second number: "))
+            mult1 = float(input("enter first number: "))
+            mult2 = float(input("enter second number: "))
             end = mult1*mult2
             print(end)
         except:
             print("enter a NUMBER")
     elif userinput == "divide":
         try:    
-            div1 = int(input("enter first number: "))
-            div2 = int(input("enter second number: "))
+            div1 = float(input("enter first number: "))
+            div2 = float(input("enter second number: "))
             dividend = div1/div2
             print(float(dividend))
         except:
             print("enter a NUMBER")
     elif userinput == "pythagoreas":
         try:
-            leg1 = int(input("enter leg 1: "))
-            leg2 = int(input("enter leg 2: "))
+            leg1 = float(input("enter leg 1: "))
+            leg2 = float(input("enter leg 2: "))
             leg3 = mth.sqrt((leg1*leg1) + (leg2*leg2))
             print(float(leg3))
         except:
             print("enter a NUMBER")
     elif userinput == "exponent":
         try:
-            base = int(input("enter base: "))
-            exponent = int(input("enter exponent: "))
+            base = float(input("enter base: "))
+            exponent = float(input("enter exponent: "))
             equals = base**exponent
             print(float(equals))
         except:
             print("enter a NUMBER")
     elif userinput == "squareroot":
         try:
-            besqrt = int(input("what do you want the root of?: "))
+            besqrt = float(input("what do you want the root of?: "))
             root = mth.sqrt(besqrt)
             print(float(root))
         except:
